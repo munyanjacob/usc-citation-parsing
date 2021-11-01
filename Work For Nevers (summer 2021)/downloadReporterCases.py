85f55c3f659421fd622fd813dea2f129dd3b70a7
munyanjacob <munyanjacob@gmail.com>
2021-11-01 03:24:43 -0400

    removed empty space

--- Work For Nevers (summer 2021)/downloadReporterCases.py
+++ Work For Nevers (summer 2021)/downloadReporterCases.py
@@ -8,18 +8,6 @@ NUM_CASES = 10000
 url = 'https://api.case.law/v1/cases/?reporter=983&full_case=true&page_size=' + str(NUM_CASES)
 responses = 0
 
-def parseResponse():
-	global responses
-	i = 1
-	data = responses["results"]
-
-	for case in data:
-		opinions = case["casebody"]["data"]["opinions"]
-		print(i)
-		i += 1
-
-	return
-
 def callNextResponse(responsesToSave):
 	global url
 	global responses
@@ -33,16 +21,12 @@ def callNextResponse(responsesToSave):
 	responses = response.json()
 
 	for case in responses["results"]:
-		opinions = case["casebody"]["data"]["opinions"]
-		if len(opinions) == 0:
-			#print(case["frontend_pdf_url"])
+		if len(case["casebody"]["data"]["opinions"]) == 0:
 			continue
-		#elif opinions[0]["text"].endswith("denied."):
 		else:
 			responsesToSave["results"].append(case)
 
 	url = responses["next"]
-	print("done")
 
 
 
@@ -52,4 +36,4 @@ while (url != None):
 	callNextResponse(responsesToSave)
 
 with open(filename, 'w') as f:
-			json.dump(responsesToSave, f)
+	json.dump(responsesToSave, f)
